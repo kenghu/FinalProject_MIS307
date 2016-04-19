1b501eea101bc7519f296661fd1e5e52cef1f9a1
bturkett <bturkett@iastate.edu>
2016-04-18 19:58:04 -0500

    Update PayRollCalculation.java
    Added some final state tax brackets to reference
    hard coded social security and medicare rates into methods
    added state tax calculations; lengthy but should work. could shorten the code

--- PayRollCalculation.java
+++ PayRollCalculation.java
@@ -9,6 +9,14 @@ public class PayRollCalculation {
 	private double socialSecurityRate;
 	private double federalRate;
 	private double stateRate;
+	final double stateOne = 1539/52; //lowest state tax bracket
+	final double stateTwo = 3078/52; //second state tax bracket
+	final double stateThree = 6156/52;
+	final double stateFour = 13851/52;
+	final double stateFive = 23085/52;
+	final double stateSix = 30780/52;
+	final double stateSeven = 46170/52; //second highest state tax bracket
+	final double stateEight = 69255/52; //highest state tax bracket
 	
 	
 	public PayRollCalculation(String name_P, double hourlyRate, double hour, double healthCare_P, double taxRate_P,double socialSecurityRate_P, double federalRate_P,double stateRate_P)
@@ -29,11 +37,13 @@ public class PayRollCalculation {
 	}
 	public double socialSecurityPay()
 	{
+		socialSecurityRate = 0.062;
 		double pay = emHourlyRate*emHour*socialSecurityRate;
 		return pay;
 	}
 	public double medicarePay()
 	{
+		dealthCareRate = 0.0145;
 		double pay = emHourlyRate*emHour*healthCareRate;
 		return pay;
 	}
@@ -44,8 +54,118 @@ public class PayRollCalculation {
 	}
 	public double statePay()
 	{
-		double pay = emHourlyRate*emHour*stateRate;
-		return pay;
+		double gross = emHourlyRate * emHour;
+		stateRate = 0.0036;
+		double stateTax = 0;
+		if(gross <= stateOne){
+			stateTax = gross * stateRate;
+		}
+		else if (gross <= stateTwo)
+		{
+			double a = (stateOne * stateRate);
+			stateRate = 0.0072;
+			double b = (gross - stateOne) * stateRate;
+			stateTax = a+b;
+		}
+		else if (gross <= stateThree){
+			double a = (stateOne * stateRate);
+			stateRate = 0.0072;
+			double b = (stateTwo * stateRate);
+			stateRate = 0.0243;
+			double c = (gross-stateTwo) * stateRate;
+			stateTax = a+b+c;
+		}
+		else if (gross <= stateFour){
+			double a = (stateOne * stateRate);
+			stateRate = 0.0072;
+			double b = (stateTwo * stateRate);
+			stateRate = 0.0243;
+			double c = (stateThree * stateRate);
+			stateRate = 0.045;
+			double d = (gross - stateThree) * stateRate;
+			stateTax = a+b+c+d;
+		}
+		else if (gross <= stateFive){
+			double a = (stateOne * stateRate);
+			stateRate = 0.0072;
+			double b = (stateTwo * stateRate);
+			stateRate = 0.0243;
+			double c = (stateThree * stateRate);
+			stateRate = 0.045;
+			double d = (stateFour * stateRate);
+			stateRate = 0.0612;
+			double e = (gross-stateFour)*stateRate;
+			stateTax =a+b+c+d+e;
+		}
+		else if (gross <= stateSix){
+			double a = (stateOne * stateRate);
+			stateRate = 0.0072;
+			double b = (stateTwo * stateRate);
+			stateRate = 0.0243;
+			double c = (stateThree * stateRate);
+			stateRate = 0.045;
+			double d = (stateFour * stateRate);
+			stateRate = 0.0612;
+			double e = (stateFive * stateRate);
+			stateRate = 0.0648;
+			double f = (gross - stateFive)*stateRate;
+			stateTax = a+b+c+d+e+f;
+		}
+		else if (gross <= stateSeven){
+			double a = (stateOne * stateRate);
+			stateRate = 0.0072;
+			double b = (stateTwo * stateRate);
+			stateRate = 0.0243;
+			double c = (stateThree * stateRate);
+			stateRate = 0.045;
+			double d = (stateFour * stateRate);
+			stateRate = 0.0612;
+			double e = (stateFive * stateRate);
+			stateRate = 0.0648;
+			double f = (stateSix * stateRate);
+			stateRate = 0.068;
+			double g = (gross - stateSix)*stateRate;
+			stateTax = a+b+c+d+e+f+g;
+		}
+		else if (gross <= stateEight){
+			double a = (stateOne * stateRate);
+			stateRate = 0.0072;
+			double b = (stateTwo * stateRate);
+			stateRate = 0.0243;
+			double c = (stateThree * stateRate);
+			stateRate = 0.045;
+			double d = (stateFour * stateRate);
+			stateRate = 0.0612;
+			double e = (stateFive * stateRate);
+			stateRate = 0.0648;
+			double f = (stateSix * stateRate);
+			stateRate = 0.068;
+			double g = (stateSeven * stateRate);
+			stateRate = 0.0792;
+			double h = (gross-stateSeven)*stateRate;
+			stateTax = a+b+c+d+e+f+g+h;
+		}
+		else {
+			double a = (stateOne * stateRate);
+			stateRate = 0.0072;
+			double b = (stateTwo * stateRate);
+			stateRate = 0.0243;
+			double c = (stateThree * stateRate);
+			stateRate = 0.045;
+			double d = (stateFour * stateRate);
+			stateRate = 0.0612;
+			double e = (stateFive * stateRate);
+			stateRate = 0.0648;
+			double f = (stateSix * stateRate);
+			stateRate = 0.068;
+			double g = (stateSeven * stateRate);
+			stateRate = 0.0792;
+			double h = (stateEight*stateRate);
+			stateRate = 0.0898;
+			double i = (gross-stateEight)*stateRate;
+			stateTax = a+b+c+d+e+f+g+h+i;
+		}
+		return stateTax;
 	}
 	public double netPay()
 	{
